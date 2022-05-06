4a897cbd369c7bd3d90e838005bcad164fc6fb57
Andrey Stelmakhgit config --global user.email andrey.stelmakh@live.rugit config --global user.namegit config --global user.name Andrey <andrey.stelmakh@live.ru>
2022-05-06 20:10:45 +0300

    Changes

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,12 @@ from django.db import models
 
 
 class Phone(models.Model):
+    # id
+    name = models.CharField(max_length=50)
+    price = models.FloatField()
+    image = models.CharField(max_length=250)
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(max_length=250)
     # TODO: Добавьте требуемые поля
     pass
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from phones.models import Phone
 
 
 def index(request):
@@ -7,11 +8,24 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort = request.GET.get('sort', 'name')
+    if sort == 'name':
+        phone_objects = Phone.objects.all().order_by('name')
+    else:
+        if sort == 'min_price':
+            phone_objects = Phone.objects.all().order_by('price')
+        else:
+            if sort == 'max_price':
+                phone_objects = Phone.objects.all().order_by('-price')
+
+    phones = [phone for phone in phone_objects]
+    context = {'phones': phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone_objects = Phone.objects.filter(slug=slug)
+    phones = [phone for phone in phone_objects]
+    context = {'phone': phones[0]}
     return render(request, template, context)
