0751061647fcbdf45968b3f6002c659393829fe9
Andrey Stelmakhgit config --global user.email andrey.stelmakh@live.rugit config --global user.namegit config --global user.name Andrey <andrey.stelmakh@live.ru>
2022-05-13 17:08:00 +0300

    Changes

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Scope, ArticleScope
+
+
+class ArticleScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        already_has_main = False
+        for form in self.forms:
+            is_main = form.cleaned_data.get('is_main', False)
+            if already_has_main is True and is_main is True:
+                raise ValidationError('В качестве основного может быть выбран только один раздел')
+            if is_main:
+                already_has_main = True
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+class ArticleScopeInline(admin.TabularInline):
+    model = ArticleScope
+    formset = ArticleScopeInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ArticleScopeInline]
+
+
+@admin.register(Scope)
+class ScopeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
     pass
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,16 +1,43 @@
 from django.db import models
 
 
+class Scope(models.Model):
+
+    title = models.CharField(max_length=256, verbose_name='Раздел')
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+        ordering = ['title']
+
+
 class Article(models.Model):
 
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    scope = models.ManyToManyField(Scope, related_name='articles', through='ArticleScope')
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+
+class ArticleScope(models.Model):
+    scope = models.ForeignKey(Scope, related_name='scopes', on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, related_name='scopes', on_delete=models.CASCADE)
+    is_main = models.BooleanField(verbose_name='Основной')
+
+    class Meta:
+        ordering = ['-is_main', 'scope__title']
+
+    def __str__(self):
+        return self.scope.title
+
+    def title(self):
+        return self.scope.title
