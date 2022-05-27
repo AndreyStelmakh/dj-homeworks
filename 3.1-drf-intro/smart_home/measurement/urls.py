4eae4503bce942e0af01ce1135a5032f6b82b335
Andrey Stelmakhgit config --global user.email andrey.stelmakh@live.rugit config --global user.namegit config --global user.name Andrey <andrey.stelmakh@live.ru>
2022-05-27 13:19:13 +0300

    Changes

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+from measurement.views import ListCreateSensors, CreateMeasurement, RetrieveUpdateSensor
+
 urlpatterns = [
     # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', ListCreateSensors.as_view()),
+    path('measurement/', CreateMeasurement.as_view()),
+    path('sensors/<pk>/', RetrieveUpdateSensor.as_view())
 ]
